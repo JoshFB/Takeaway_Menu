b6f0d3055f733b2f47c76c6bdd65f2e267c42adb
Josh <jfb1@hotmail.co.uk>
2014-06-11 11:57:27 +0100

    completed text message output

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,5 +5,16 @@ def self.show
 	ITEMS
 end
 
+def display_menu
+	puts "Select item"
+	@select_item = gets.chomp
+end
+
+
+def add_items_to_order
+	@dish << @select_item
+end
+
+
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,8 +1,14 @@
 require 'menu'
 
 describe Menu do
+let (:menu) {Menu.new}
+
 	it "can show a list of items" do
 		expect(Menu.show.first[:name]).to eq "Egg fried rice"
 	end
+
+	it "can display the menu to user" do 
+		
+		end
 
 end
